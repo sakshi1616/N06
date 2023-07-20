ea8c65e7a8efe235582b4d1554d693459a3ae249
Lenovo <Lenovo@LAPTOP-6A8R0EEP>
2023-07-20 14:07:59 +0530

    first commit

---
+++ dayone/src/org/tnsif/introduction/CompilerInput.java
@@ -0,0 +1,12 @@
+//program to demonstrate on compile -time-input
+package org.tnsif.introduction;
+
+public class CompilerInput {
+
+	public static void main(String[] args) {
+		//variable initialization
+		short num = 20;
+		System.out.println("The num is: " +num);
+	}
+
+}
---
+++ dayone/src/org/tnsif/introduction/UserInputDemo.java
@@ -0,0 +1,16 @@
+//program to demonstrate on user input value
+package org.tnsif.introduction;
+
+import java.util.Scanner;
+
+public class UserInputDemo {
+	
+	public static void main(String[] args) {
+
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter a string: ");
+	String strl=s.next();
+	System.out.println("string is: "+strl);
+
+}
+}
