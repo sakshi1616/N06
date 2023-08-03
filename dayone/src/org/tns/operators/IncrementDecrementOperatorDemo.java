90caad2cd13357b8877112cbd41c82ad8de6bb54
Lenovo <Lenovo@LAPTOP-6A8R0EEP>
2023-08-03 12:53:34 +0530

    second commit

--- dayone/src/org/tns/operators/IncrementDecrementOperatorDemo.java
+++ dayone/src/org/tns/operators/IncrementDecrementOperatorDemo.java
@@ -4,7 +4,7 @@ package org.tns.operators;
 public class IncrementDecrementOperatorDemo {
 
 	public static void main(String[] args) {
-	 int x=20,y=5;
+	 int x=20,y=2;
 	 int result= x++ + ++y;
 	 System.out.println(result);
 	 System.out.println(x);
